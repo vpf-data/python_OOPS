b6d5e5fb3684eabb8356a82cd4e2d84bfad04ab2
vpf-data <vivekpf.bi@gmail.com>
2025-05-09 16:50:55 +0530

    introduced tuple as return

--- oops_lecture.py
+++ oops_lecture.py
@@ -1,15 +1,12 @@
 def main():
 
-    name = get_name()
-    house = get_house()
-    print(f"{name} is in {house}")
+    student=get_student()
+    print(f"{student[0]} is in {student[1]}")
 
-def get_name():
-    return input("Enter name :")
-
-def get_house():
-    return input("Enter house :")
-
+def get_student():
+    name = input("Enter name: ")
+    house = input("Enter house: ")
+    return (name,house)
 if __name__ =="__main__":
     main()
 
