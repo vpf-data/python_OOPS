15e425f9c629b175801d119ff71c5f07663c63f8
vpf-data <vivekpf.bi@gmail.com>
2025-05-12 10:08:45 +0530

    introduced dictionary as return

--- oops_lecture.py
+++ oops_lecture.py
@@ -1,14 +1,13 @@
 def main():
 
     student=get_student()
-    if student[0]=="padma":
-        student[1]="ravenclaw"
-    print(f"{student[0]} is in {student[1]}")
+    print(f"{student['name']} is in {student['house']}")
 
 def get_student():
-    name = input("Enter name: ").lower()
-    house = input("Enter house: ").lower()
-    return [name,house]
+    student={}
+    student["name"] = input("Enter name: ").lower()
+    student["house"] = input("Enter house: ").lower()
+    return student
 if __name__ =="__main__":
     main()
 
