41f674e62e79baabdceeac20d08469d5b8716d9c
vpf-data <vivekpf.bi@gmail.com>
2025-05-12 09:52:57 +0530

    tuple is replaced by a list , since list is mutable and modifications can be done.

--- oops_lecture.py
+++ oops_lecture.py
@@ -1,12 +1,14 @@
 def main():
 
     student=get_student()
+    if student[0]=="padma":
+        student[1]="ravenclaw"
     print(f"{student[0]} is in {student[1]}")
 
 def get_student():
-    name = input("Enter name: ")
-    house = input("Enter house: ")
-    return (name,house)
+    name = input("Enter name: ").lower()
+    house = input("Enter house: ").lower()
+    return [name,house]
 if __name__ =="__main__":
     main()
 
