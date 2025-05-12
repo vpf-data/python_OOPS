dac5b7926228f1a497077eb275209ac3ca79daff
vpf-data <vivekpf.bi@gmail.com>
2025-05-12 10:19:18 +0530

    given dictionary as return on the fly , instead of declaring first.

--- oops_lecture.py
+++ oops_lecture.py
@@ -1,13 +1,14 @@
 def main():
 
     student=get_student()
+    if student["name"]=="padma":
+        student["house"]="ravenclaw"
     print(f"{student['name']} is in {student['house']}")
 
 def get_student():
-    student={}
-    student["name"] = input("Enter name: ").lower()
-    student["house"] = input("Enter house: ").lower()
-    return student
+    name = input("Enter name: ").lower()
+    house= input("Enter house: ").lower()
+    return {"name":name,"house":house}
 if __name__ =="__main__":
     main()
 
