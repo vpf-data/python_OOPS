e2f56392b5e8aa0d72bd992d0576bcc8e5e4f1b0
vpf-data <vivekpf.bi@gmail.com>
2025-05-13 10:42:13 +0530

    Added __str__ function to Student class , so in main function when print object is done, str function will be called.

--- oops_lecture.py
+++ oops_lecture.py
@@ -8,10 +8,13 @@ class Student:
         self.name=name
         self.house=house
 
+    def __str__(self):
+        return f"{self.name} is from {self.house}"
+
 def main():
 
     wizard=get_student()
-    print(f"{wizard.name} is in {wizard.house}")
+    print(wizard)
 
 def get_student():
     name = input("Enter name: ").lower()
