3017774bd29d57685bae560bad6ea235bdea7cbe
vpf-data <vivekpf.bi@gmail.com>
2025-05-21 11:57:26 +0530

    introduced class method

--- hat.py
+++ hat.py
@@ -1,10 +1,12 @@
 import random
 
 class Hat:
-    def __init__(self):
-        self.houses = ["Gryffindor","Hufflepuff","RavenClaw","Slytherin"]
-    def sort(self,name):
-        print(name,"is in",random.choice(self.houses))
 
-hat=Hat()
-hat.sort("Harry")
+   houses = ["Gryffindor","Hufflepuff","RavenClaw","Slytherin"]
+
+   @classmethod
+   def sort(cls,name):
+        print(name,"is in",random.choice(cls.houses))
+
+
+Hat.sort("Harry")
