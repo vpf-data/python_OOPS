37c9e5ddd3f57ed337c4e80299fd46b542b86cd1
vpf-data <vivekpf.bi@gmail.com>
2025-05-21 11:55:10 +0530

    initial commit

---
+++ hat.py
@@ -0,0 +1,10 @@
+import random
+
+class Hat:
+    def __init__(self):
+        self.houses = ["Gryffindor","Hufflepuff","RavenClaw","Slytherin"]
+    def sort(self,name):
+        print(name,"is in",random.choice(self.houses))
+
+hat=Hat()
+hat.sort("Harry")
